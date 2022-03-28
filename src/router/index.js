7f914b974b77437345cd812de3177d6cd72783b2
ashwin kc <ashwinkalaichelvan29@gmail.com>
2022-03-28 16:48:43 +0800

    ashwin using github

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Login from '../components/Login.vue';
 import Dashboard from '../components/Dashboard.vue';
 import Register from '../views/Register.vue';
 import Home from '../views/Home.vue';
-import CreatePQ from '../views/CreatePQ';
+import CreatePQ from '../views/CreatePQ.vue';
 import ProfilePage from '../views/ProfilePage';
 import MyPQPage from '../views/MyPQPage';
 import ChatsPage from '../views/ChatsPage';
