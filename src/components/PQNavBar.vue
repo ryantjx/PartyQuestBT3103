f69d1f0731bd7a4540ae262842fb47452c211cc5
ryantjx <76402618+ryantjx@users.noreply.github.com>
2022-04-05 21:01:19 +0800

    Update Explore and Profile Display

---
+++ src/components/PQNavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="logged" v-if="user">
+        <div id="nav">
+            <router-link to="/mypq/CreatedPQPage">Created PQs</router-link> |
+            <router-link to="/mypq/JoinedPQPage">Joined PQs</router-link> |
+            <router-link to="/mypq/saved">Saved PQs</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export default {
+    name: 'PQNavBar',
+
+    data() {
+        return {
+            user: false,
+        };
+    },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                this.user = user;
+            }
+        });
+    },
+};
+</script>
+
+<style>
+#nav {
+    padding: 30px;
+    text-align: center;
+}
+#nav a {
+    font-weight: bold;
+    color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+    color: #42b983;
+}
+</style>
